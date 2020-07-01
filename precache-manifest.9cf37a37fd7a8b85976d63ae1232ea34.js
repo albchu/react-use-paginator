a9c5b8cd1b14dcf1f555d18210d7874f35dcb702
Albert <albertchu539@gmail.com>
2020-06-30 21:37:37 -0600

    Updates

---
+++ precache-manifest.9cf37a37fd7a8b85976d63ae1232ea34.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7bfe56efad9c3f53b557c9d0350865c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "bb77a1899412ac1b7cca",
+    "url": "./static/css/main.729e284e.chunk.css"
+  },
+  {
+    "revision": "256948098a28bc7bfb68",
+    "url": "./static/js/2.70fbaa14.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.70fbaa14.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb77a1899412ac1b7cca",
+    "url": "./static/js/main.ae864155.chunk.js"
+  },
+  {
+    "revision": "8d943d1546b8e4183ebe",
+    "url": "./static/js/runtime-main.45a7cb31.js"
+  }
+]);
